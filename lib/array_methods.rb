5ebdec0b235abdc5ef8de4c91b0292a67aecb851
Andrea Toston <tostonar@gmail.com>
2019-07-22 14:53:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,9 +4,8 @@ def find_element_index(array, value_to_find)
   array.length.times do |index|
     if array[index] == value_to_find
        return index
-    else
-       return nil
     end
+    return nil
   end
 end
 
